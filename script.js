8df1ce85f256a586858d26bf9d6fe992a7f1f099
SPDTeeKoo <soumyadutta150@gmail.com>
2023-01-23 15:09:53 +0530

    1st_commit

---
+++ script.js
@@ -0,0 +1,33 @@
+setTimeout(() => {
+  document.body.innerHTML = '10';
+  setTimeout(() => {
+    document.body.innerHTML = '09';
+    setTimeout(() => {
+      document.body.innerHTML = '08';
+      setTimeout(() => {
+        document.body.innerHTML = '07';
+        setTimeout(() => {
+          document.body.innerHTML = '06';
+          setTimeout(() => {
+            document.body.innerHTML = '05';
+            setTimeout(() => {
+              document.body.innerHTML = '04';
+              setTimeout(() => {
+                document.body.innerHTML = '03';
+                setTimeout(() => {
+                  document.body.innerHTML = '02';
+                  setTimeout(() => {
+                    document.body.innerHTML = '01';
+                    setTimeout(() => {
+                      document.body.innerHTML = 'HAPPY INDEPENDENCE DAY!!';
+                    }, 2000);
+                  }, 2000);
+                }, 2000);
+              }, 2000);
+            }, 2000);
+          }, 2000);
+        }, 2000);
+      }, 2000);
+    }, 2000);
+  }, 2000);
+}, 2000);
